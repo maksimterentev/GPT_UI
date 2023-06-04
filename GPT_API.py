718a3da80aeeeb81ce7764fc0bdd3789165027b8
terentevmn <112945851+terentevmn@users.noreply.github.com>
2023-06-04 20:08:43 +0200

    GPT_API 1.2.0

--- GPT_API.py
+++ GPT_API.py
@@ -1,21 +1,21 @@
 # Maksim Terentev
 # GPT API
 # Last changes: 04/06/2023
-# Version 1.1.1
+# Version 1.2.0
 
 import openai
 
 def conversation(model, system_content, user_content, max_tokens = 50, temperature = 0):
     if model == "text-davinci-003":
-        completion = openai.Completion.create(
+        response = openai.Completion.create(
             engine = model,
             prompt = system_content + user_content,
             max_tokens = max_tokens,
             temperature = temperature,
             logprobs = 3)
-        return completion.choices[0].text.strip()
+        return response.choices[0].text.strip()
     elif model == "gpt-3.5-turbo" or model == "gpt-4":
-        completion = openai.ChatCompletion.create(
+        response = openai.ChatCompletion.create(
             model = model,
             max_tokens = max_tokens,
             temperature = temperature,
@@ -23,7 +23,7 @@ def conversation(model, system_content, user_content, max_tokens = 50, temperatu
             messages = [
                 #{"role": "system", "content": system_content}, 
                 {"role": "user", "content": system_content + user_content}])
-        return completion['choices'][0]['message']['content'] 
+        return response['choices'][0]['message']['content'] 
     
     
 
